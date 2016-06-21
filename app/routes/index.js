a218a759657e09694079c2602f30cb535dc92d6a
cory <whippoorwill.in.flight@gmail.com>
2016-06-21 15:22:12 -0700

    add new answer

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('answer');
   },
+
+  actions: {
+    save3(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    }
+  }
 });
