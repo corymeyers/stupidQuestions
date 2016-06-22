4ae83bd0bd258d660e6f1040bbf570c0a3be2ac2
cory <whippoorwill.in.flight@gmail.com>
2016-06-22 12:10:46 -0700

    start code for favorite answer list

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoritesList: Ember.inject.service(),
+
+  actions: {
+    addToFavorite(item) {
+      console.log(item);
+      this.get('favoritesList').add(item);
+
+    }
+  }
 });
