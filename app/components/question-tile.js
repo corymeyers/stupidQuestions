60d860da451f768e6c631df6ecb2313c237678e8
cory <whippoorwill.in.flight@gmail.com>
2016-06-22 11:28:46 -0700

    add computed property to concatinate question and author

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   updateQuestionForm: false,
+
+  fullQuestion: Ember.computed('question.head', 'question.author', function() {
+    return this.get('question.head') + ' Original question asked by ' + this.get('question.author');
+  }),
+
   actions: {
     update(question, params) {
       this.sendAction('update', question, params);
